776e5a140c7b3ada160695ba3a638633dc75acaa
Legends11 <235496468+tickwarden@users.noreply.github.com>
2026-07-06 19:09:29 +0000

    .

--- scripts/onvert_normal_datapack.sh
+++ scripts/onvert_normal_datapack.sh
@@ -1,3 +1,3 @@
-rm -f -rf datapack/RTWrapper-Datapack/data/runtoolkit/function
-rm -f -rf datapack/RTWrapper-Datapack/data/rtwrapper/function/tests
-sed -i '/scoreboard objectives add rtw.test dummy/d' datapack/RTWrapper-Datapack/data/rtwrapper/function/core/load.mcfunction
+sudo rm -rf datapack/RTWrapper-Datapack/data/runtoolkit/function
+sudo rm -rf datapack/RTWrapper-Datapack/data/rtwrapper/function/tests
+sudo sed -i '/scoreboard objectives add rtw.test dummy/d' datapack/RTWrapper-Datapack/data/rtwrapper/function/core/load.mcfunction
