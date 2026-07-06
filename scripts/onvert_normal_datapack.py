4bc45c7bdad77efe7b0baa548297008690cba87e
Legends11 <235496468+tickwarden@users.noreply.github.com>
2026-07-06 19:18:07 +0000

    .

--- scripts/onvert_normal_datapack.py
+++ scripts/onvert_normal_datapack.py
@@ -1,3 +1,5 @@
 import subprocess
+
+print("Converting...")
 convertToNormalDatapack = subprocess.run(["bash", "scripts/convert_normal_datapack.sh"], capture_output=True, text=True)
-print("Converting...", convertToNormalDatapack.stdout)
+convertToNormalDatapack()
